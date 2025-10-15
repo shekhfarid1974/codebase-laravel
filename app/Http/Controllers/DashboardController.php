22dc080ebe0bfa7dcadebaec84d492ac366bfaad
shekhfarid1974 <shekhfarid1974@gmail.com>
2025-10-15 11:40:54 +0600

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+// app/Http/Controllers/DashboardController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display the dashboard view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+}
