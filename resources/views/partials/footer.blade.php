1a3a8b1578dd6db1cd4429386447cdaeda6754cf
shekhfarid1974 <shekhfarid1974@gmail.com>
2025-10-16 17:37:29 +0600

    name change in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
         © 2025 MY Outsourcing Ltd.
     </div>
     <div class="footer-center">
-        Powered by Laravel 10 • Built by Sheikh Farid
+        Powered by Laravel 10 • Built by Shekh Farid
     </div>
     <div class="footer-right">
         <span class="system-health">v1.0.0 • System Health ✅</span>
